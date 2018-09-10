b95b99bb7d50e5460f7a90404576cb0220ec04f6
kutzer <kutzer@usna.edu>
2018-09-10 07:29:44 -0400

    Update to ScorIsMoving

--- ScorBotToolboxFunctions/ScorIsMoving.m
+++ ScorBotToolboxFunctions/ScorIsMoving.m
@@ -1,4 +1,4 @@
-function isMoving = ScorIsMoving()
+function [isMoving,errStruct] = ScorIsMoving()
 % SCORISMOVING checks if the ScorBot is executing a move.
 %   isMoving = SCORISMOVING returns 1 if ScorBot is executing a move and 0
 %   otherwise.
@@ -7,13 +7,41 @@ function isMoving = ScorIsMoving()
 %       
 %   M. Kutzer, 10Aug2015, USNA
 
+% Updates
+%   06Sep2018 - Updated to identify specific error codes that stop movement
+
 %% Check ScorBot and define library alias
-[isReady,libname] = ScorIsReady;
+% Check ScorBot
+[isReady,libname,errStruct] = ScorIsReady;
+
+% Display critical errors
+dispFlag = 'Display Critical';
+if errStruct.Code ~= 0
+    %fprintf(2,'ScorIsMoving Error Catch: ');
+    ScorDispError(errStruct);%,dispFlag);
+end
+
+% Return if robot is not ready
 if ~isReady
     isMoving = false;
     return
 end
 
+% Define special-case error codes that stop the ScorBot
+switch errStruct.Code
+    case 201
+        isMoving = false;
+        return
+    case 903
+        isMoving = false;
+        return
+    otherwise
+        if isnan(errStruct.Code)
+            isMoving = false;
+            return
+        end
+end
+        
 %% Check if ScorBot is moving
 switch calllib(libname,'RIsMotionDone')
     case 0 % ScorBot is moving
