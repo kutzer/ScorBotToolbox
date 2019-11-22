596cce8aeeb0356a81cd0236d282bdf0c5f7ba57
Michael Kutzer <kutzer@usna.edu>
2019-11-22 12:00:38 -0500

    Fixed documentation and z-height

--- ScorBotToolboxFunctions/ScorDrawMaker.m
+++ ScorBotToolboxFunctions/ScorDrawMaker.m
@@ -1,12 +1,20 @@
 function ScorDrawMaker
 % SCORDRAWMAKER initializes a visualization of the ScorBot and renders an
-% image of Professor Emeritus Carl Wick. Carl continues to be instrumental 
-% in the development of all versions of the ScorBot Toolbox his creation 
-% of RobotDll back in 2010 for the Matlab Toolbox for the Intelitek Scorbot
-% (MTIS), his USBC version fix in 2019 leading to the addition and use of 
-% ScorConfigurationSync.m in the ScorBot Toolbox, and his development of
-% the 64bit update to the toolbox that enables full use of the documented
-% functionality of the Intelitek USB library. 
+% image of Professor Emeritus Carl Wick. 
+%
+% Carl continues to be instrumental in the development of all versions of 
+% the ScorBot Toolbox. He released the original RobotDll back in 2010 for 
+% the "Matlab Toolbox for the Intelitek Scorbot" (MTIS). This library was 
+% the jumping point for all MATLAB interaction with the Intelitek 
+% SCORBOT-ER 4u at the United States Naval Academy. Since that time, Carl 
+% has answered countless questions and even identified the critical USBC 
+% version fix during the summer of 2019. This fix led to the addition and 
+% use of ScorConfigurationSync.m, solving issues associated with the 
+% recurring 201 impact errors that plagued the toolbox during its rocky, 
+% yearlong transition to 64-bit OS. In the time since, he has developed a 
+% 64bit update to the toolbox that will supersede the existing toolbox 
+% framework and enable full use of the new, documented functionality of 
+% the Intelitek USBC library. 
 %
 %   Thank you for everything Carl!
 %
@@ -18,7 +26,6 @@ imEND = imread('Wick.png');
 % Crop image for uniform aspect ratio
 imEND = imcrop(imEND,[16,19,2399,2699]);
 % Resize image
-%imEND = imresize(imEND,(1/6));
 imEND = imresize(imEND,(1/9));
 % Make image "color" for rendering
 imEND = repmat(imEND,1,1,3);
@@ -32,7 +39,6 @@ sim = ScorSimInit;
 set(sim.Axes,'Visible','off');
 xlim(sim.Axes,[-225,525]);
 ylim(sim.Axes,[-225,225]);
-%set(sim.Figure,'Units','Inches','Position',[0.5,0.5,10,6],'Color',[1,1,1]);
 set(sim.Figure,'Units','Normalized','Position',[0,0,1,1],'Color',[1,1,1],...
     'Name','Professor Emeritus C. Wick, USNA Department of Weapons, Robotics, & Control Engineering');
 ScorSimPatch(sim);
@@ -56,23 +62,24 @@ end
 % Define image transformation
 H_p2o = Ry(pi)*Tx(-(size(imEND,2) + 140) )*Ty(-(size(imEND,1)/2) )*Tz(-105);
 h_p2o = triad('Parent',sim.Frames(1),'Matrix',H_p2o,'Scale',50);
-% Render image
+% Render image and place in simulation
 figTMP = figure;
 axsTMP = axes('Parent',figTMP);
 img = imshow(imNOW,'Parent',axsTMP);
 set(img,'Parent',h_p2o);
 delete(figTMP);
 
-%% Define drawing parameters
-% Initialize drawing pose
+%% Initialize ScorBot end-effector pose for drawing
 R0 = ...
     [ 1, 0, 0;...
     0,-1, 0;...
     0, 0,-1];
 H0 = eye(4);
 H0(1:3,1:3) = R0;
+
+%% Define drawing parameters
 % Define drawing waypoints
-r = 7; % Closted gripper tip is ~7mm wide
+r = 7; % Closed gripper tip is ~7mm wide
 y_Pass = (r+1):(2*r):size(imEND,1);
 for i = 1:numel(y_Pass)
     switch mod(i,2)
@@ -99,51 +106,72 @@ for i = 1:numel(y_Pass)
 end
 
 %% Define path information
-s0 = 0;
-d = -r:(r-1);
-xALL = [];
-yALL = [];
-zALL = [];
-sALL = [];
-dsALL = [];
+s0 = 0;         % Initial cumulative 1/(avg. brightness + 1) value
+d = -r:(r-1);   % Sketch "diameter"
+xALL = [];      % All x-positions for path
+yALL = [];      % All y-positions for path
+zALL = [];      % All z-positions for path
+dsALL = [];     % All average brightness values for path
+sALL = [];      % All cumulative 1/(avg. brightness + 1) values for path
+
 for i = 1:numel(xyzWPT_p)
+    % Total number of discrete values between x_init & x_goal
     n = abs( diff(xyzWPT_p{i}(1,:)) ) + 1;
+    % Interpolate x/y/z initial and goal values
     x = linspace(xyzWPT_p{i}(1,1),xyzWPT_p{i}(1,2),n);
     y = linspace(xyzWPT_p{i}(2,1),xyzWPT_p{i}(2,2),n);
     z = linspace(xyzWPT_p{i}(3,1),xyzWPT_p{i}(3,1),n);
     
+    % Define average brightness and cumulative 1/(avg. brightness + 1)
+    % -> Average y-value
     y0 = mean(y);
     for j = 1:numel(x)
+        % -> Mean average brightness over for sketch "diameter"
         ds(j) = mean(mean( double(imEND(d+y0,x(j))) ));
+        % -> Cumulative 1/(avg. brightness + 1)
         s(j) = s0 + 1/(ds(j)+1);
+        % -> Update current cumulative 1/(avg. brightness + 1)
         s0 = s(j);
     end
     
+    % Update path arrays
     xALL = [xALL,x];
     yALL = [yALL,y];
     zALL = [zALL,z];
-    sALL = [sALL,s];
     dsALL = [dsALL,ds];
+    sALL = [sALL,s];
 end
 
-%% Convert "s" to time
-t_max = 10; % seconds
+%% Convert cumulative 1/(avg. brightness + 1) values ("s") to time
+% -> We want to go slower for darker pixel averages and faster for lighter
+%    pixel averages.
+t_max = 10; % Desired total time to complete the drawing (seconds)
 s2t = t_max/max(sALL);
 tALL = sALL * s2t;
 
-%% Convert "ds" to z
-z_min = -5;
-p = polyfit([0,255],[z_min,0],1);
-zALL = polyval(p,dsALL);
+%% Convert average brightness values ("ds") to z height
+% -> We want to press more (harder) for average brightness values closer 
+%    to 0, and press less for average brightness values closer to 255.
 
-%% Spline for path
+% Given that the z-direction of Carl's image points into the working
+% surface, a more negative z-value represents a lighter interaction and a
+% z-value of zero represents the "hardest" interaction possible.
+%     0 - Black --> 0 (i.e. push hardest) height
+%   255 - White --> z_min (negative) height 
+z_min = -5; % minimum height
+p = polyfit([0,255],[0,z_min],1); % linear fit
+zALL = polyval(p,dsALL); % z-heights
+
+%% Fit a time-dependent function for the x/y/z path
 pp(1) = spline(tALL,xALL);
 pp(2) = spline(tALL,yALL);
 pp(3) = spline(tALL,zALL);
 
 %% Animate
 % Define points in time
-fps = 30;
+%   Frame p - relative to Carl's picture
+%   Frame o - relative to the ScorBot base frame
+fps = 30;   % assumed animation frame rate (frames per second)
 t = [0:(1/fps):t_max,t_max];
 for i = 1:numel(pp)
     X_p(i,:) = ppval(pp(i),t);
@@ -167,9 +195,11 @@ end
 
 % Draw Carl
 for f = 1:size(X_p,2)
+    % Move ScorBot
     H0(1:3,4) = X_o(1:3,f);
     ScorSimSetPose(sim,H0);
     
+    % Update image mask
     D = (xALL - X_p(1,f)).^2 + (yALL - X_p(2,f)).^2 + (zALL - X_p(3,f)).^2;
     idx = find(D == min(D));
     bin = false(size(imNOW));
@@ -177,13 +207,14 @@ for f = 1:size(X_p,2)
         bin(d+yALL(k),xALL(k),:) = true;
     end
     
+    % Update image
     imNOW(bin) = imEND(bin);
     set(img,'CData',imNOW);
     
     drawnow;
 end
 
-% Move to home
+% Move to home position
 qi = ScorSimGetBSEPR(sim);
 n = 20;
 q = [];
