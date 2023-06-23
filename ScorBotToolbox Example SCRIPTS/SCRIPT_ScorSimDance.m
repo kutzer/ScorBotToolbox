115a807dcd868701061161bc1349fa7c1d68106f
Michael Kutzer <kutzer@usna.edu>
2023-06-23 12:30:22 -0400

    Update SCRIPT_ScorSimDance.m

--- ScorBotToolbox Example SCRIPTS/SCRIPT_ScorSimDance.m
+++ ScorBotToolbox Example SCRIPTS/SCRIPT_ScorSimDance.m
@@ -3,6 +3,8 @@
 %
 %   M. Kutzer 18Feb2016, USNA
 
+OPTION = 1;
+
 sim = ScorSimInit;
 ScorSimPatch(sim);
 
@@ -11,12 +13,20 @@ k = 1.0; % Allow movements up to a factor of k times the total distance between 
 while true
     q_init = transpose( ScorSimGetBSEPR(sim) );
     q_goal = transpose( ScorBSEPRRandom(k) );
-    T = [q_init, q_goal];
-    S = [s_vec(0), s_vec(1)];
-    M = T*S^(-1);
-    
-    s = linspace(0,1,round( 30*norm(diff(T,1,2)) ));
-    for s_i = s
-        ScorSimSetBSEPR(sim, transpose( M*s_vec(s_i) ) );
+
+    switch OPTION
+        case 1
+            % OPTION 1 - Use the embedded ScorSim* interpolation
+            ScorSimSetBSEPR(sim,q_goal,'MoveType','LinearJoint');
+            ScorSimWaitForMove(sim);
+        case 2
+            % OPTION 2 - Manually interpolate and use 'Instant' 'MoveType'
+            T = [q_init, q_goal];
+            S = [s_vec(0), s_vec(1)];
+            M = T*S^(-1);
+            s = linspace(0,1,round( 30*norm(diff(T,1,2)) ));
+            for s_i = s
+                ScorSimSetBSEPR(sim, transpose( M*s_vec(s_i) ),'MoveType','Instant' );
+            end
     end
 end
