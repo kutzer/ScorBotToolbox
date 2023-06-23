bf1269ffafdc6401a0874669f2a0616a970a6322
Michael Kutzer <kutzer@usna.edu>
2023-06-23 12:38:13 -0400

    Update SCRIPT_ScorSimDance.m

--- ScorBotToolbox Example SCRIPTS/SCRIPT_ScorSimDance.m
+++ ScorBotToolbox Example SCRIPTS/SCRIPT_ScorSimDance.m
@@ -3,24 +3,42 @@
 %
 %   M. Kutzer 18Feb2016, USNA
 
+% Update(S)
+%   23Jun2023 - Added OPTION to show embedded interpolation vs manual
+%               interpolation options
+
+%% Define option
 OPTION = 1;
 
+%% Initialize simulation
 sim = ScorSimInit;
 ScorSimPatch(sim);
 
+%% Animate random movements
+
+% Allow movements up to a factor of k times the total distance between limits
+k = 1.0; 
+
+% Define parameters for manual interpolation
 s_vec = @(s) [s; 1];
-k = 1.0; % Allow movements up to a factor of k times the total distance between limits
+
 while true
     q_init = transpose( ScorSimGetBSEPR(sim) );
     q_goal = transpose( ScorBSEPRRandom(k) );
 
     switch OPTION
         case 1
-            % OPTION 1 - Use the embedded ScorSim* interpolation
+            % OPTION 1 - Use the embedded ScorSim* linear joint interpolation
             ScorSimSetBSEPR(sim,q_goal,'MoveType','LinearJoint');
             ScorSimWaitForMove(sim);
         case 2
-            % OPTION 2 - Manually interpolate and use 'Instant' 'MoveType'
+            % OPTION 2 - Use the embedded ScorSim* linear task
+            %            interpolation
+            ScorSimSetBSEPR(sim,q_goal,'MoveType','LinearTask');
+            ScorSimWaitForMove(sim);
+        case 3
+            % OPTION 3 - Manually interpolate in joint space and use 
+            %            'Instant' 'MoveType'
             T = [q_init, q_goal];
             S = [s_vec(0), s_vec(1)];
             M = T*S^(-1);
